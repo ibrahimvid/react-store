acb678e421040aeda85826a574160247252309dc
ibrahimvid <ivdinli@gmail.com>
2020-07-21 12:55:00 -0700

    seperated the app into client folder, added express backend for stripe

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51Gqo42JiAHmnU2VnKI0Pg8rAINlgzoerEsyrhKofI7GWWiJOeqWe8pUofy6JP7tCmEL4XpQl2DPuLdi363Txsft700ohjpiHSs';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert('Payment Succesful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert('There was an issue with your payment, Please use the provided card.');
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Store React LLC.'
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Production Setup
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+// Stripe key
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+// Server Setup for Heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // For any route, send the index.html
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log(`Server running at ${port}`);
+});
+
+// Payment route
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
